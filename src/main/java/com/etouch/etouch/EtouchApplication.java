0790f39de8e471f1557d1645ff8c1929bba46190
huguodong <531035580@qq.com>
2018-11-29 10:05:53 +0800

    1

---
+++ src/main/java/com/etouch/etouch/EtouchApplication.java
@@ -0,0 +1,23 @@
+package com.etouch.etouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class EtouchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtouchApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+}
+
+
+
